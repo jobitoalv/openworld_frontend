772287159eba9fe64ed1a779bc51a0acfd9d044b
Job Alvarez <jobitoalv@gmail.com>
2023-01-19 11:59:05 +1000

    create contanier and componets folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Login from './components/login'
+import Home from './container/Home'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-        hello job
-    </h1>
+    <Routes>
+        <Route path='login' element={<Login />} />
+        <Route path='/*' element={<Home />} /> 
+    </Routes>
   )
 }
 
