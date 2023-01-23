80c2fbda23fd620137ae12245ec0bdfa86012519
Job Alvarez <jobitoalv@gmail.com>
2023-01-23 15:02:48 +1000

    Feed file created

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { IoMdAdd, IoMdSearch } from 'react-icons/io';
+// import { IoMdAdd, IoMdSearch } from 'react-icons/io';
 
 const Navbar = ({ searchTerm, setSearchTerm, user }) => {
   const navigate = useNavigate();
@@ -9,7 +9,7 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
     return (
       <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7 ">
         <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
-          <IoMdSearch fontSize={21} className="ml-1" />
+          <section fontSize={21} className="ml-1" />
           <input
             type="text"
             onChange={(e) => setSearchTerm(e.target.value)}
@@ -24,7 +24,7 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
             <img src={user.image} alt="user-pic" className="w-14 h-12 rounded-lg " />
           </Link>
           <Link to="/create-pin" className="bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center">
-            <IoMdAdd />
+            <section />
           </Link>
         </div>
       </div>
